8834381c6f30d3c8facbd886b3aececd982cad6d
Lyenx <105422453+ImLyenx@users.noreply.github.com>
2024-02-28 11:39:03 +0100

    added cors + updated home page

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./router.js";
 import cards from "./routes/CardRoutes.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(router);
